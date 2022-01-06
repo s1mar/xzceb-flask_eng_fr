092e532c4d085295ecf7bcddc8bcc9f324fd2162
Simar <sk8537@rit.edu>
2022-01-06 13:09:44 -0500

    Update

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -1,6 +1,5 @@
+import translator
 import unittest
-from machinetranslation import translator
-
 class Test_Translator(unittest.TestCase):
     def test_null_englishToFrench(self):
         self.assertEqual(translator.english_to_french(''),"Please enter text to translate")
@@ -13,5 +12,13 @@ class Test_Translator(unittest.TestCase):
 
     def test_EnglishForBonjour(self):
         self.assertEqual(translator.french_to_english('Bonjour'),'Hello')
-    
+
+    def test_frenchToEnglish(self):
+        self.assertEqual(translator.french_to_english('Bonjour'),'Hello')
+        self.assertNotEqual(translator.french_to_english('Bonjour'),'Bye')
+
+    def test_englishToFrench(self):
+        self.assertEqual(translator.english_to_french('Hello'),'Bonjour')
+        self.assertNotEqual(translator.english_to_french('Bye'),'Bonjour')
+        
 unittest.main()
